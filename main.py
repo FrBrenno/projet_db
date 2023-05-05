39c24a4c4c3d20b481de417786ad1df2329c55b5
alexbien <alexandre.bienfait@hotmail.com>
2023-05-06 00:45:57 +0200

    insertion de tous les fichiers csv dans la base de donnée

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import mysql.connector
 import csv
 
@@ -12,21 +14,64 @@ db = mysql.connector.connect(
 mycursor = db.cursor()
 mycursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
 
-#mycursor.execute("DROP TABLE IF EXISTS dossiers_patients")
-#mycursor.execute(
-    #"CREATE TABLE dossiers_patients (NISS_patient BIGINT, medecin TEXT, inami_medecin BIGINT, pharmacien TEXT, inami_pharmacien BIGINT, medicament_nom_commercial TEXT, DCI TEXT, date_prescription DATE, date_vente DATE, duree_traitement INTEGER)")
+#creation des tables
 
-mycursor.execute("INSERT INTO dossiers_patients (NISS_patient, medecin, inami_medecin, pharmacien, inami_pharmacien, medicament_nom_commercial, DCI, date_prescription, date_vente, duree_traitement) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (1, "medecin", 1, "pharmacien", 1, "medicament_nom_commercial", "DCI", "2021-01-01", "2020-02-02", 1))
-db.commit()
-"""with open("data/dossiers_patients.csv", encoding='utf-8') as csvfile:
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS dossiers_patients (NISS_patient BIGINT, medecin TEXT, inami_medecin "
+                 "BIGINT, pharmacien TEXT, inami_pharmacien BIGINT, medicament_nom_commercial TEXT, DCI TEXT, "
+                 "date_prescription DATE, date_vente DATE, duree_traitement INTEGER)")
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS medicaments (dci TEXT,nom_Commercial TEXT,système_anatomique TEXT,"
+                 "conditionnement INT)")
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS pathologies (maladie TEXT, systemes TEXT)")
+
+# enlève les données des tables si elles existent déjà
+mycursor.execute("DELETE FROM dossiers_patients")
+mycursor.execute("DELETE FROM medicaments")
+mycursor.execute("DELETE FROM pathologies")
+
+# insertion des données dans les tables
+
+
+with open("data/dossiers_patients.csv", encoding='utf-8') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     for row in csvreader:
-        print(row)
+
         if row[0] != "NISS_patient":
+
+            # convertir les dates mois/jour/annee au format date annee-mois-jour
+            date_prescription_mauvais_format = row[7]
+            date_obj_prescri = datetime.datetime.strptime(date_prescription_mauvais_format, "%m/%d/%Y")
+            row[7] = date_obj_prescri.strftime("%Y-%m-%d")
+
+            date_vente_mauvais_format = row[8]
+            date_obj_vente = datetime.datetime.strptime(date_vente_mauvais_format, "%m/%d/%Y")
+            row[8] = date_obj_prescri.strftime("%Y-%m-%d")
+
+
+            mycursor.execute("INSERT INTO dossiers_patients (NISS_patient, medecin, inami_medecin, pharmacien, inami_pharmacien, medicament_nom_commercial, DCI, date_prescription, date_vente, duree_traitement) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",row)
+
+
+with open("data/medicaments.csv", encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader:
+        if row[0] != "dci":
+
+            mycursor.execute("INSERT INTO medicaments (dci, nom_Commercial, système_anatomique, conditionnement) VALUES (%s, %s, %s, %s)",row)
+
+
+with open("data/pathologies.csv", encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row in csvreader:
+        if row[0] != "maladie":
+
             mycursor.execute(
-                "INSERT INTO dossiers_patients (NISS_patient, medecin, inami_medecin, pharmacien, inami_pharmacien, medicament_nom_commercial, DCI, date_prescription, date_vente, duree_traitement) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                "INSERT INTO pathologies (maladie, systemes) VALUES (%s, %s)",
                 row)
 
 
-"""
+# Sauvegarder les modifications
+db.commit()
+
 
