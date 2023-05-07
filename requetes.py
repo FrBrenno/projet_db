d361e18796432ea124b36010e27b7424f5db9b5d
alexbien <alexandre.bienfait@hotmail.com>
2023-05-07 16:02:31 +0200

    début application graphique

--- requetes.py
+++ requetes.py
@@ -13,21 +13,28 @@ mycursor = db.cursor()
 def requete1():
     mycursor.execute(
         "SELECT dci, nom_Commercial, conditionnement FROM medicaments WHERE dci = 'ibuprofene' ORDER BY nom_Commercial ASC, conditionnement ASC")
+    res = ""
     for x in mycursor:
-        print(x)
-
+        res += str(x) + "\n"
+    print(res)
+    return res
 def requete2():
     # La liste des pathologies qui peuvent être prise en charge par un seul type de spécialistes.
     mycursor.execute("SELECT maladie FROM pathologies GROUP BY maladie HAVING COUNT(maladie) = 2")
+    res = ""
     for x in mycursor:
-        print(x)
-
+        res += str(x) + "\n"
+    print(res)
+    return res
 def requete3():
          #La spécialité de médecins pour laquelle les médecins prescrivent le plus de médicaments.
-    mycursor.execute("SELECT specialite, COUNT(*) AS occurences FROM diagnostiques GROUP BY specialite ORDER BY occurences DESC LIMIT 15")
+    mycursor.execute("SELECT specialite, COUNT(*) AS occurences FROM diagnostiques GROUP BY specialite ORDER BY occurences DESC LIMIT 1")
+    res = ""
     for x in mycursor:
-        print(x)
+        res += str(x) + "\n"
+    print(res)
+    return res
 
 #requete1()
 #requete2()
-requete3()
+#requete3()
