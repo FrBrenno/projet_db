a52cd724c053fc9a3dd296d3cd9372a2610c5638
Alexandre <alexandre.bienfait@hotmail.com>
2023-05-25 15:45:53 +0200

    fix requête 3

--- requetes/requete_3.sql
+++ requetes/requete_3.sql
@@ -1,5 +1,6 @@
-SELECT specialite, COUNT(*) 
-AS occurences 
-FROM diagnostiques 
-GROUP BY specialite 
-ORDER BY occurences DESC LIMIT 1
+SELECT medecins.specialite,
+COUNT(*) AS occurences
+FROM dossiers_patients JOIN medecins
+WHERE dossiers_patients.inami_medecin = medecins.inami
+GROUP BY medecins.specialite
+ORDER BY occurences DESC LIMIT 100
