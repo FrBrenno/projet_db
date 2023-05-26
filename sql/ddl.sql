7177bb3d14f93d36adbb6d75dbd539662bc00693
Philippe Mutkowski <95864752+mtkski@users.noreply.github.com>
2023-05-26 12:32:29 +0200

    DDL

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -6,3 +6,5 @@ CREATE TABLE IF NOT EXISTS patients (NISS BIGINT, nom TEXT, prenom TEXT, date_de
 CREATE TABLE IF NOT EXISTS pharmaciens (inami BIGINT, nom TEXT, mail TEXT, telephone BIGINT)
 CREATE TABLE IF NOT EXISTS diagnostiques (NISS BIGINT, date_diagnostic DATE, naissance DATE, pathologie TEXT, specialite TEXT)
 CREATE TABLE IF NOT EXISTS specialites (nom TEXT, systeme_anatomique TEXT)
+-- Doit être représenté ligne par ligne pour fonctionner avec mycursor.execute()
+-- Voir le fichier "DDL_formaté.sql" pour un script + lisible
