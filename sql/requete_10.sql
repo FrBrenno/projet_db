c669cda97557f881cb4559acaaec8388d9d1f8c3
alexbien <alexandre.bienfait@hotmail.com>
2023-05-26 15:37:55 +0200

    re fix requete 10

--- sql/requete_10.sql
+++ sql/requete_10.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT DCI
-FROM mydatabase.dossiers_patients dp1
+FROM dossiers_patients dp1
 WHERE DCI NOT IN(
 	SELECT DCI
-    FROM mydatabase.dossiers_patients dp2
-    WHERE dp1.DCI = dp2.DCI AND dp2.date_prescription >= '2001-07-30'
+    FROM dossiers_patients dp2
+    WHERE dp1.DCI = dp2.DCI AND dp2.date_prescription >= %s
 )
