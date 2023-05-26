9af30dfc376192614a06120f22d0d0c27698adc9
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-05-26 16:08:11 +0200

    Update requete_7.sql

--- sql/requete_7.sql
+++ sql/requete_7.sql
@@ -1,18 +1,16 @@
-SELECT decade, MAX(most_consumed_medication) AS most_frequent_medication
+SELECT 
+	decade, 
+    most_consumed_medication
 FROM (
-    SELECT 
-        CONCAT((YEAR(t1.date_de_naissance) DIV 10)*10, '-', (YEAR(t1.date_de_naissance) DIV 10)*10 + 9) AS decade,
-        t2.DCI AS most_consumed_medication
-    FROM patients t1
-    JOIN (
-        SELECT p.date_de_naissance, dp.DCI, COUNT(*) AS medication_count
-        FROM dossiers_patients dp, patients p 
-        WHERE dp.NISS_patient = p.NISS
-        GROUP BY p.date_de_naissance, dp.DCI
-    ) AS t2 ON YEAR(t2.date_de_naissance) >= YEAR(t1.date_de_naissance) DIV 10 * 10
-        AND YEAR(t2.date_de_naissance) <= YEAR(t1.date_de_naissance) DIV 10 * 10 + 9
-    WHERE YEAR(t1.date_de_naissance) BETWEEN 1950 AND 2020
-    GROUP BY decade, most_consumed_medication
-) AS subquery
-GROUP BY decade
-
+  SELECT 
+    CONCAT((YEAR(p.date_de_naissance) DIV 10)*10, '-', (YEAR(p.date_de_naissance) DIV 10)*10 + 9) AS decade,
+    d.DCI AS most_consumed_medication,
+    ROW_NUMBER() OVER (PARTITION BY CONCAT((YEAR(p.date_de_naissance) DIV 10)*10, '-', (YEAR(p.date_de_naissance) DIV 10)*10 + 9) ORDER BY COUNT(*) DESC) AS rn
+  FROM dossiers_patients d
+  JOIN patients p ON d.NISS_patient = p.NISS
+  WHERE YEAR(p.date_de_naissance) BETWEEN 1950 AND 2020
+  GROUP BY CONCAT((YEAR(p.date_de_naissance) DIV 10)*10, '-', (YEAR(p.date_de_naissance) DIV 10)*10 + 9), d.DCI
+) as subquery
+WHERE rn = 1
+GROUP BY decade, most_consumed_medication
+ORDER BY decade ASC;
